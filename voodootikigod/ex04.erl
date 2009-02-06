3fbd620a83c548d36d5a4b243d2db77ed8cf094a
Chris Williams <chris@iterativedesigns.com>
2009-02-06 14:33:07 -0500

    fixed up.

--- voodootikigod/ex04.erl
+++ voodootikigod/ex04.erl
@@ -6,7 +6,8 @@
 
 % API Functions
 on_init()     ->
-  register_client().
+  find_others(),
+  register_me().
   
 on_message(Message, State)    ->
   process_message(Message,State).
@@ -17,8 +18,14 @@ on_terminate()  ->
 start() ->
   simple_server:start(ex04).
   
+  
+  
 % local functions
-register_client() ->
+
+find_others() ->
+  lists:map(fun(X) -> net_adm:ping(X) end, ['chef@Clever-Computer-Name', 'boorad@boorad']).
+
+register_me() ->
   Me = node(),
   global:trans({?RECV, ?RECV}, 
   	fun() ->
@@ -37,6 +44,9 @@ process_message(Message, State) ->
     {chat_msg, From, Msg} ->
       io:format("Message from ~p: ~p~n",[From,string:strip(Msg, both, $\n)]),
       {reply, ok, State};
+    {chat_msg, Msg} ->
+      io:format("Anonymous Message: ~p~n",[string:strip(Msg, both, $\n)]),
+      {reply, ok, State};
     Fail  ->
       io:format("STFU: ~p~n", [Fail]),
       stop
@@ -44,7 +54,7 @@ process_message(Message, State) ->
   
 send_message(Server, Message) ->
   Pid = global:whereis_name(Server),
-  simple_server:call(Pid, Message).
+  simple_server:call(Pid, {chat_msg, Message}).
   
 stop(Server)  ->
   Pid = global:whereis_name(Server),
